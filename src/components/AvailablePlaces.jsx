d440a0cd1f44f9b09f7c92f17b2e223c5c70c77d
Edoardo <edoardoderosaa@gmail.com>
2025-05-28 20:10:54 +0200

    Aggiunto stato per attendere la visualizzazione dei dati

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,27 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+      setIsLoading(false);
+    }
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Loading places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
